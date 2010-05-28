f7e767b37b809dc9637b22c2145330254859af26
mauro <mauro@mauro-desktop>
2010-05-28 14:48:05 +0200

    added event views

--- src/dam/workflow/models.py
+++ src/dam/workflow/models.py
@@ -20,20 +20,22 @@ from django.db import models
 
 class State(models.Model):
 	name = models.CharField(max_length=256)
-#	workspace = 
+	workspace = models.ForeignKey('workspace.DAMWorkspace')
 	
-#	def save(self, *args, **kwargs):
-#		from eventmanager.models import Event
-#		super(State, self).save(*args, **kwargs)
-#		Event.objects.create(name = 'state')
-		
+	class Meta:
+		unique_together = (('name', 'workspace'),)
+	
+	def save(self, *args, **kwargs):
+		from eventmanager.models import Event
+		super(State, self).save(*args, **kwargs)
+		Event.objects.create(name = 'state change to '+ self.name, workspace = self.workspace)
 	
 	def __unicode__(self):
 		return self.name	
 
 class StateItemAssociation(models.Model):
 	state = models.ForeignKey(State)
-	workspace = models.ForeignKey('workspace.DAMWorkspace')
+#	workspace = models.ForeignKey('workspace.DAMWorkspace')
 	item = models.ForeignKey('repository.Item')
 	
 	def save(self, *args, **kwargs):
