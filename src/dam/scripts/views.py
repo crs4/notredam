1a5ff2a9c6928ece8341f6ed376bc3abba6955b4
mdrio <mauro@crs4.it>
2010-07-12 14:08:23 +0200

    minor changes

--- src/dam/scripts/views.py
+++ src/dam/scripts/views.py
@@ -119,8 +119,8 @@ def _new_script(name = None, description = None, workspace = None, pipeline = No
         pipeline = simplejson.loads(pipeline)
     
         for media_type, actions in pipeline.items():
-            source_variant_pk = actions.get('source_variant',  'original')
-            source_variant = Variant.objects.get(pk = source_variant_pk, auto_generated = False )
+            source_variant_name = actions.get('source_variant',  'original')
+            source_variant = Variant.objects.get(name = source_variant_name, auto_generated = False )
             ActionList.objects.create(script = script, media_type = Type.objects.get(name = media_type), actions = simplejson.dumps(actions), source_variant = source_variant)
 
     
