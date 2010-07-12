0e5e18480837a39341c297f03cb09d2ebad2aba9
mdrio <mauro@crs4.it>
2010-07-12 10:47:35 +0200

    fixing scripts (save action with rendition id )

---
+++ src/dam/metadata/admin.py
@@ -0,0 +1,27 @@
+#########################################################################
+#
+# NotreDAM, Copyright (C) 2009, Sardegna Ricerche.
+# Email: labcontdigit@sardegnaricerche.it
+# Web: www.notre-dam.org
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#########################################################################
+
+from django.contrib import admin
+from dam.metadata.models import MetadataProperty, MetadataDescriptor, MetadataDescriptorGroup, MetadataValue, RightsValue, RightsXMPValue
+
+admin.site.register(MetadataProperty)
+admin.site.register(MetadataDescriptor)
+admin.site.register(MetadataDescriptorGroup)
+admin.site.register(MetadataValue)
+admin.site.register(RightsValue)
+admin.site.register(RightsXMPValue)
