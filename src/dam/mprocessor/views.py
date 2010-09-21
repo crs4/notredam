35f0508d521050451134f136521446a7daf3e64f
orlando murru <orlando.murru@gmail.com>
2010-09-21 18:43:14 +0200

    ck7

--- src/dam/mprocessor/views.py
+++ src/dam/mprocessor/views.py
@@ -15,11 +15,18 @@ def job_dispatch(request, job_id):
     """General dispatcher for incoming responses"""
     try:
         logger.debug("-------------------request.raw_post_data: %s" %request.raw_post_data)
-        result = loads(request.raw_post_data)
+        post_data = loads(request.raw_post_data)
         job = Job.objects.get(job_id=job_id)
     except Exception, e:
         logger.debug('-----------------------------------------------Invalid body in post: %s' % str(e))
         return HttpResponseServerError('invalid data');
+    if 'result' in post_data:
+        result = post_data['result']
+    elif 'error' in post_data:
+        logger.error('%s returned and error: %s:' % job.decoded_params[0], post_data['error'])
+        result = {} 
+    else:
+        logger.error('Invalid jsonrpc response')
     if job.next():
         job.execute(result)
         logger.debug("-----job.next")
