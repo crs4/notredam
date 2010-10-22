066501da036d296ec96f730fdd29423bebc44ae7
orlando murru <orlando.murru@gmail.com>
2010-10-22 03:09:22 +0200

    chk

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,12 @@
 import os
 import sys
 import subprocess
+from uuid import uuid4
 from distutils.core import setup, Command, DistutilsOptionError
 from distutils.command.install import install as _install
+from distutils.file_util import move_file
+from distutils.dir_util import mkpath
+from shutil import copyfile
 
 # The python packages that this setup is going to install
 package_list = [
@@ -31,6 +35,19 @@ package_list = [
 'dam/workspace',
 ]
 
+package_data = {
+'dam/workspace': ['fixtures/initial_data.json'],
+'dam/metadata': ['fixtures/initial_data.json'],
+'dam/scripts': ['fixtures/initial_data.json'],
+'dam/eventmanager': ['fixtures/initial_data.json'],
+'dam/variants': ['fixtures/initial_data.json'],
+'dam/core/dam_repository': ['fixtures/initial_data.json'],
+'dam/core/dam_metadata': ['fixtures/initial_data.json'],
+'dam': ['fixtures/initial_data.json', 'LICENSE.TXT', ],
+'dam/treeview': ['fixtures/initial_data.json'],
+'dam/preferences': ['fixtures/initial_data.json'], 
+}
+
 # Names of required debian packages. Each entry specifies:
 # <package_name>  <list of args of apt-get install (version numbers etc.)>
 debian_packages = [
@@ -43,71 +60,78 @@ tarfiles = [
 # ('python-txamqp_0.3.tgz', 'python-txamqp-0.3', ['sudo', 'python', 'setup.py', 'install']),
 ]
 
+#############################################################################################3
 
 
-class InstallDep(Command):
-    description = "install required debian and tarball packages"
-    user_options = []
-    def initialize_options(self): pass
-    def finalize_options(self): pass
+#
+# Utility functions
+#
+def _run(cmdline, as_root, failures):
+    if as_root:
+        cmdline.insert(0, 'sudo')
+    ret = subprocess.call(cmdline)
+    if ret > 0:
+        failures.append('Error executing %s' % ' '.join(cmdline))
+    return ret
 
-    def install_tar(self, failures):
-        curdir = os.getcwd()
-        for tarfile, topdir, exe in tarfiles:
-            print ('\n######## Installing %s' % tarfile)
-            clean_up = topdir and not os.path.exists(topdir)  # delete new directories
-            ret = subprocess.call(['tar', 'xvzf', tarfile])
-            if ret != 0:
-                failures.append('Installation error for %s: unable to untar' % tarfile)
-                return
-            if topdir:
-                os.chdir(topdir)
-            ret = subprocess.call(exe)
-            if ret != 0:
-                failures.append('Installation error for %s: unable to install' % tarfile)
-                return
-            os.chdir(curdir)
-            if clean_up:
-                print 'Removing %s' % topdir
-                subprocess.call(['sudo', 'rm', '-rf', topdir])
+def _install_tar(failures):
+    "install the tarballs"
+    curdir = os.getcwd()
+    for tarfile, topdir, exe in tarfiles:
+        print ('\n######## Installing %s' % tarfile)
+        clean_up = topdir and not os.path.exists(topdir)  # delete new directories
+        _run(['tar', 'xvzf', tarfile], 1, failures)
+        if topdir and os.path.exists(topdir):
+            os.chdir(topdir)
+            _run(exe, 1, failures)
+        os.chdir(curdir)
+        if clean_up:
+            print 'Removing %s' % topdir
+            _run(['rm', '-rf', topdir], 1, failures)
 
-    def run(self):
-        failures = []
-        response = raw_input('\nDependencies:%s%s\nInstall? (y/N): ' % (
-              '\n - '.join([''] + debian_packages), '\n - '.join([''] + [x[0] for x in tarfiles])))
-        if not response or response.lower().startswith('n'):
-            print('Aborting installation of dependencies')
-            return
-        for pkg in debian_packages:
-            print ('\n Installing %s' % pkg)
-            ret = subprocess.call(['sudo', 'apt-get', 'install', pkg])
-            if ret != 0:
-                failures.append('Installation error for %s' % pkg)
-        self.install_tar(failures)
-        if failures:
-            print >>sys.stderr, '\nSome packages failed to install:'
-            print >>sys.stderr, '\n'.join(failures)
 
-class Config(Command):
-    description = "create configuration directory and copies cfg files"
-    user_options = [('cfgroot=', None, 'directory for .cfg files'),
-                    ('settings=', None, 'can specify default-dev, default, or mqsql')]
+def _install_debian(failures):
+    "install the debian packages"
+    response = raw_input('\nDependencies:%s%s\nInstall? (y/N): ' % (
+          '\n - '.join([''] + debian_packages), '\n - '.join([''] + [x[0] for x in tarfiles])))
+    if not response or response.lower().startswith('n'):
+        print('Aborting installation of dependencies')
+        return
+    for pkg in debian_packages:
+        print ('\n Installing %s' % pkg)
+        _run(['apt-get', 'install', pkg], 1, failures)
+    _install_tar(failures)
 
+def _install_scripts(destination, scripts, failures):
+    for script in scripts:
+        filename = os.path.basename(os.path.normpath(script))
+        target = os.path.join(destination, filename)
+        ret = _run(['cp', '-f', script, target], 1, failures)
+        if ret == 0:
+            _run(['chmod', '555', target], 1, failures)
+
+def _print_errors(failures):
+    if failures:
+        print >>sys.stderr, '\n### Errors during installation:'
+        print >>sys.stderr, '\n'.join(failures)
+
+#
+# Custom commands
+#
+class install(_install):
+    user_options = _install.user_options + [ ('settings=', None, 'can specify default-dev, default, or mqsql'),]
+    
     def initialize_options(self):
-        self.cfgroot = None
+        _install.initialize_options(self)
         self.settings = None
+        self.as_root = 1
+        self.cfgroot = os.path.join(os.sep, 'etc', 'mediadart')
 
     def finalize_options(self):
-        if not self.cfgroot:
-            if 'HOME' in os.environ:
-                self.cfgroot = os.path.join(os.environ['HOME'], '.mediadart')
-            else:
-                raise DistutilsOptionError('Missing value for configuration directory')
-        if not self.settings:
+        _install.finalize_options(self)
+        if not self.settings or self.settings == 'default.dev':
             self.settings_module = os.path.join('dam', 'settings.py.default.dev')
-        elif self.settings == 'default.dev':
-            self.settings_module = os.path.join('dam', 'settings.py.default.dev')
-        elif self.settings == 'default.dev':
+        elif self.settings == 'default':
             self.settings_module = os.path.join('dam', 'settings.py.default')
         elif self.settings == 'mysql':
             self.settings_module = os.path.join('dam', 'settings.py.mysql')
@@ -115,19 +139,37 @@ class Config(Command):
             raise DistutilsOptionError('Invalid value for --settings: %s' % self.settings)
 
     def run(self):
+        failures = []
+        _install_debian(failures)
+        _install.run(self)
+        if os.path.exists(os.path.join('dam', 'settings.py')):
+            os.unlink(os.path.join('dam', 'settings.py'))
+        self.copy_file(self.settings_module, os.path.join(self.install_purelib, 'dam', 'settings.py'))
         self.copy_file(self.settings_module, os.path.join('dam', 'settings.py'))
+        if self.install_purelib != os.path.dirname(os.path.abspath(__file__)):
+            self.copy_tree(os.path.join('dam', 'files'), os.path.join(self.install_purelib, 'dam', 'files'))
+            self.copy_tree(os.path.join('dam', 'templates'), os.path.join(self.install_purelib, 'dam', 'templates'))
         if not os.path.exists(self.cfgroot):
             raise DistutilsModuleError('Missing mediadart configuration directory: %s' % self.cfgroot)
-        self.copy_file('dam/001-notredam.cfg', self.cfgroot)
+        _run(['cp', '-f', os.path.join('dam', '001-notredam.cfg'), self.cfgroot], self.as_root, failures)
         from dam.settings import dir_log
         if not os.path.exists(dir_log):
             self.mkpath(dir_log)
+        _print_errors(failures)
 
-class install(_install):
-    def run(self):
-        self.run_command('install_dep')
-        _install.run(self)
-        self.run_command('config')
+class local_install(install):
+    description = "install leaving the source tree in-place: need to set PYTHONPATH"
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        self.install_purelib = os.path.dirname(os.path.abspath(__file__))
+        if 'HOME' in os.environ:
+            self.cfgroot = os.path.join(os.environ['HOME'], '.mediadart')
+            self.as_root = 0
+        else:
+            cfgroot = os.path.join(os.sep, 'etc', 'mediadart')
+            self.as_root = 1
+
 
 setup(name='notredam', 
       version='1.0.1', 
@@ -139,7 +181,8 @@ setup(name='notredam',
       platforms=['Unix'],
       url='http://www.notredam.org', 
       packages= package_list,
+      package_data = package_data,
       scripts=[],
-      cmdclass={'install_dep':  InstallDep,  'install': install, 'config': Config, },
+      cmdclass={'install':  install,  'local_install': local_install,},
      )
 
