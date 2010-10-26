8bc7e56abc03e8a415f6e3644b62c6a68a7442c9
mauro <mauro@localhost>
2010-10-26 15:11:39 +0200

    fixed logo and header

--- src/dam/files/ext/header.js
+++ src/dam/files/ext/header.js
@@ -1,22 +1,29 @@
 var header;
 Ext.onReady(function(){
    Ext.QuickTips.init();
+   
+    var items = [
+            new Ext.BoxComponent({ // raw
+                region:'center',
+                el: 'logo',
+                height:35
+            })
+            
+       ];
+       
+   if (Ext.get('toolbar_container'))
+   
+   		items.push(new Ext.BoxComponent({ // raw
+                region:'south',
+                el: 'toolbar_container',
+                height:25
+            }));
+   
     header = new Ext.Panel({
        layout: 'border', 
        region:'north',
        height:HEADERHEIGHT,
-       items: [
-            new Ext.BoxComponent({ // raw
-                region:'north',
-                el: 'logo',
-                height:35
-            }),
-            new Ext.BoxComponent({ // raw
-                region:'center',
-                el: 'north',
-                height:20
-            })
-       ]
+       items: items
    });
 
 });
