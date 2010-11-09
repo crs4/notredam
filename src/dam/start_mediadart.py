fb9f3bfbfe17d50e45bca80d30d370bf531a8073
mdrio <mauro@crs4.it>
2010-11-09 11:56:26 +0100

    new deb ready

--- src/dam/start_mediadart.py
+++ src/dam/start_mediadart.py
@@ -22,20 +22,22 @@
 #
 import os
 import sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'dam.settings'
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'dam.settings'
 
 from django.core.management import setup_environ
+import settings
+setup_environ(settings)
 from django.db.models.loading import get_models
 from mediadart import log
 from mediadart.config import Configurator
 from mediadart.launch import main
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
-setup_environ(settings)
+#try:
+#    import settings # Assumed to be in the same directory.
+#except ImportError:
+#    import sys
+#    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+#    sys.exit(1)
+
 
 def check_notredam_config():
     c = Configurator()
