a4aa89f70413ad12229698aad73256ca99617fd6
mdrio <mauro@crs4.it>
2010-11-11 13:36:49 +0100

    fixed some js syntax errors, added get_items in smart folder api

---
+++ src/dam/workflow/admin.py
@@ -0,0 +1,25 @@
+#########################################################################
+#
+# NotreDAM, Copyright (C) 2009, Sardegna Ricerche.
+# Email: labcontdigit@sardegnaricerche.it
+# Web: www.notre-dam.org
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#########################################################################
+
+from django.contrib import admin
+
+from dam.workflow.models import State, StateItemAssociation
+
+admin.site.register(State)
+#admin.site.register(StateItemAssociation)
+
