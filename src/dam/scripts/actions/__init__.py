d148f71d1c9e8b21eb2875cb8fc07acebd7daf96
mdrio <mauro@crs4.it>
2010-12-14 16:17:11 +0100

    actions in progress

--- src/dam/scripts/actions/__init__.py
+++ src/dam/scripts/actions/__init__.py
@@ -9,20 +9,14 @@ class BaseAction(object):
     media_type_supported = ['image', 'video', 'audio', 'doc']
     verbose_name = ''
     def __init__(self, input, **params):   
-        self._input = input #could be a variant or an Action
+        self.input = input #could be a variant or an Action
         self.params = params
         self._output = None
         
         
     def get_output(self):
         return self._output
-    
-    def get_input(self):
-        if isinstance(self._input, Component):
-            return self._input
-        else:
-            return self._input.get_input()
-    
+   
     def execute(self, save_as = None, format = None):
         pass
     
