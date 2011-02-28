625fe35647c6389bffb49fb018fd1ae79f3f14cb
mdrio <mauro@crs4.it>
2011-02-28 17:32:39 +0100

    some gui

--- src/dam/plugins/common/utils.py
+++ src/dam/plugins/common/utils.py
@@ -9,3 +9,11 @@ def save_type(ctype, component):
     metadataschema_mimetype = MetadataProperty.objects.get(namespace__prefix='dc',field_name='format')
     MetadataValue.objects.create(schema=metadataschema_mimetype, content_object=component, value=mime_type)
 
+def get_variants(workspace, media_type = None):
+    from django.db.models import Q
+    from dam.variants.models import Variant
+    tmp_variants = Variant.objects.filter(Q(workspace = workspace) | Q(workspace__isnull = True),  hidden = False)
+    if media_type:
+        tmp_variants = tmp_variants.filter(media_type__name = media_type)
+     
+    return [[variant.name] for variant in tmp_variants]
