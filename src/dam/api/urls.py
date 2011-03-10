329c4b72ff5b286e03db6cd8898ef2ac61712fbb
mdrio <mauro@crs4.it>
2011-03-10 10:46:45 +0100

    fixing olm bugs

---
+++ src/dam/api/urls.py
@@ -0,0 +1,138 @@
+#########################################################################
+#
+# NotreDAM, Copyright (C) 2009, Sardegna Ricerche.
+# Email: labcontdigit@sardegnaricerche.it
+# Web: www.notre-dam.org
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#########################################################################
+
+from django.conf.urls.defaults import *
+from dam.api.views import WorkspaceResource,  ItemResource,  KeywordsResource,  CollectionResource, VariantsResource,   Auth,  SmartFolderResource,  ScriptResource, StateResource
+
+
+
+urlpatterns = patterns('',
+   
+    url(r'^api/item/(.+)/get/$', ItemResource(permitted_methods=('GET'),).read),
+    url(r'^api/item/(.+)/add_to_workspace/$', ItemResource(permitted_methods=('GET', ), ).add_to_ws),
+    url(r'^api/item/(.+)/delete_from_workspace/$', ItemResource(permitted_methods=('GET',),).delete),
+#    url(r'^api/item/(.+)/set_public/$', ItemResource(permitted_methods=('POST',  ),).set_public),
+    url(r'^api/item/(.+)/set_metadata/$', ItemResource(permitted_methods=('POST')).set_metadata),
+    url(r'^api/item/(.+)/remove_metadata/$', ItemResource(permitted_methods=('POST')).remove_metadata),
+   url(r'^api/item/new/$', ItemResource(permitted_methods=('POST',), ).create),
+   
+   url(r'^api/item/(.+)/add_keywords/$', ItemResource(permitted_methods=('POST',), ).add_keywords),
+   url(r'^api/item/(.+)/remove_keywords/$', ItemResource(permitted_methods=('POST',), ).remove_keywords),
+   
+   url(r'^api/item/(.+)/add_to_collection/$', ItemResource(permitted_methods=('POST',), ).add_to_collection),
+   url(r'^api/item/(.+)/remove_from_collection/$', ItemResource(permitted_methods=('POST',), ).remove_from_collection),
+   
+   url(r'^api/item/(.+)/generate_renditions/$', ItemResource(permitted_methods=('POST',), ).generate_variants),
+   url(r'^api/private/item/(.+)/generate_renditions/$', ItemResource(permitted_methods=('POST',), private= True).generate_variants),
+   
+   url(r'^api/item/(.+)/upload/$', ItemResource(permitted_methods=('POST',), ).upload_variant),
+   url(r'^api/item/(.+)/add_component/$', ItemResource(permitted_methods=('POST'),).add_component),
+   
+#   url(r'^api/item/(.+)/(.+)/get/$', ItemResource(permitted_methods=('POST',), ).get_variant),
+   url(r'^api/item/(.+)/get_state/$', ItemResource(permitted_methods=('POST'),).get_state),
+   url(r'^api/item/(.+)/get_all_states/$', ItemResource(permitted_methods=('POST'),).get_all_states),
+   url(r'^api/item/(.+)/set_state/$', ItemResource(permitted_methods=('POST'),).set_state),
+   
+
+#   url(r'^api/item/search/$', ItemResource(permitted_methods=('POST'),).search),
+   url(r'^api/item/(.+)/get_keywords/$', ItemResource(permitted_methods=('GET'),).get_keywords),
+   url(r'^api/item/(.+)/get_collections/$', ItemResource(permitted_methods=('GET'),).get_collections),
+   
+
+
+   
+   url(r'^api/workspace/new/$', WorkspaceResource(permitted_methods=('POST',), ).create),   
+   
+   url(r'^api/workspace/(\d+)/delete/$', WorkspaceResource(permitted_methods=('GET',), ).delete),  
+   url(r'^api/workspace/(\d+)/edit/$', WorkspaceResource(permitted_methods=('POST')).edit), 
+   url(r'^api/workspace/(\d+)/set_name/$', WorkspaceResource(permitted_methods=('POST')).set_name),  
+   url(r'^api/workspace/(\d+)/set_description/$', WorkspaceResource(permitted_methods=('POST')).set_description),  
+   
+   url(r'^api/workspace/(\d+)/get/$', WorkspaceResource(permitted_methods=('GET',), ).read),  
+   url(r'^api/workspace/get/$', WorkspaceResource(permitted_methods=('GET',), ).get_list),  
+   url(r'^api/workspace/(\d+)/get_keywords/$', WorkspaceResource(permitted_methods=('GET',), ).get_keywords),  
+   url(r'^api/workspace/(\d+)/get_items/$', WorkspaceResource(permitted_methods=('GET',), ).get_items),  
+     
+   url(r'^api/workspace/(\d+)/get_renditions/$', WorkspaceResource(permitted_methods=('GET',), ).get_variants),  
+   url(r'^api/workspace/(\d+)/get_collections/$', WorkspaceResource(permitted_methods=('GET',), ).get_collections),  
+   url(r'^api/workspace/(\d+)/get_members/$', WorkspaceResource(permitted_methods=('GET',), ).get_members),  
+   url(r'^api/workspace/(\d+)/add_members/$', WorkspaceResource(permitted_methods=('POST',), ).add_members),  
+   url(r'^api/workspace/(\d+)/remove_members/$', WorkspaceResource(permitted_methods=('POST',), ).remove_members),  
+   url(r'^api/workspace/(\d+)/set_permissions/$', WorkspaceResource(permitted_methods=('POST',), ).set_permissions),  
+    
+   url(r'^api/workspace/(\d+)/get_smartfolders/$', WorkspaceResource(permitted_methods=('GET'), ).get_smartfolders),
+   url(r'^api/workspace/(\d+)/get_scripts/$', WorkspaceResource(permitted_methods=('GET'), ).get_scripts),
+   url(r'^api/workspace/(\d+)/set_creator/$', WorkspaceResource(permitted_methods=('POST',), ).set_creator),  
+   url(r'^api/workspace/(\d+)/get_items_complete/$', WorkspaceResource(permitted_methods=('GET',), ).get_items_complete),  
+   url(r'^api/workspace/(\d+)/search/$', WorkspaceResource(permitted_methods=('POST',), ).search),
+   
+   url(r'^api/workspace/(\d+)/get_states/$', WorkspaceResource(permitted_methods=('GET',),).get_states),
+   
+   
+   url(r'^api/keyword/new/$', KeywordsResource(permitted_methods=('POST',  ), ).create),   
+   url(r'^api/keyword/(\d+)/delete/$', KeywordsResource(permitted_methods=('GET',  ), ).delete),   
+   url(r'^api/keyword/(\d+)/move/$', KeywordsResource(permitted_methods=('GET',  ), ).move),   
+   url(r'^api/keyword/(\d+)/edit/$', KeywordsResource(permitted_methods=('POST',  ), ).edit),   
+   url(r'^api/keyword/(\d+)/get/$', KeywordsResource(permitted_methods=('GET',), ).read),  
+   url(r'^api/keyword/(\d+)/add_items/$', KeywordsResource(permitted_methods=('POST')).add_items),   
+   url(r'^api/keyword/(\d+)/remove_items/$', KeywordsResource(permitted_methods=('POST'), ).remove_items),      
+#   url(r'^api/keyword/get/$', KeywordsResource(permitted_methods=('GET',  ), )),   
+   
+   
+   url(r'^api/collection/new/$', CollectionResource(permitted_methods=('POST'),  ).create),   
+   url(r'^api/collection/new/$', CollectionResource(permitted_methods=('POST')).create),   
+   url(r'^api/collection/(\d+)/delete/$', CollectionResource(permitted_methods=('GET',  ), ).delete),   
+   url(r'^api/collection/(\d+)/move/$', CollectionResource(permitted_methods=('POST',  ), ).move),   
+   url(r'^api/collection/(\d+)/edit/$', CollectionResource(permitted_methods=('POST')).edit),   
+   url(r'^api/collection/(\d+)/get/$', CollectionResource(permitted_methods=('GET',), ).read),      
+#   url(r'^api/collection/get/$', CollectionResource(permitted_methods=('GET',), ).read),      
+   url(r'^api/collection/(\d+)/add_items/$', CollectionResource(permitted_methods=('POST')).add_items),   
+   url(r'^api/collection/(\d+)/remove_items/$', CollectionResource(permitted_methods=('POST'), ).remove_items),   
+   
+   url(r'^api/rendition/new/$', VariantsResource(permitted_methods=('POST'), ).create),   
+   url(r'^api/rendition/(\d+)/get/$', VariantsResource(permitted_methods=('GET'), ).read),   
+#   url(r'^api/rendition/get/$', VariantsResource(permitted_methods=('GET'), ).read),   
+   url(r'^api/rendition/(\d+)/edit/$', VariantsResource(permitted_methods=('POST'), ).edit),   
+   url(r'^api/rendition/(\d+)/delete/$', VariantsResource(permitted_methods=('GET'), ).delete),   
+  
+   url(r'^api/smartfolder/new/$', SmartFolderResource(permitted_methods=('POST'), ).create),   
+   url(r'^api/smartfolder/(\d+)/get/$', SmartFolderResource(permitted_methods=('GET')).read),   
+#   url(r'^api/smartfolder/get/$', SmartFolderResource(permitted_methods=('GET'), ).read),   
+   url(r'^api/smartfolder/(\d+)/edit/$', SmartFolderResource(permitted_methods=('POST'), ).edit),   
+   url(r'^api/smartfolder/(\d+)/delete/$', SmartFolderResource(permitted_methods=('GET'), ).delete),   
+   url(r'^api/smartfolder/(\d+)/get_items/$', SmartFolderResource(permitted_methods=('POST'), ).get_items),   
+#   
+   url(r'^api/script/new/$', ScriptResource(permitted_methods=('POST'), ).create),   
+   url(r'^api/script/(\d+)/run/$', ScriptResource(permitted_methods=('POST'), ).run),
+   url(r'^api/script/(\d+)/run_again/$', ScriptResource(permitted_methods=('POST'), ).run_again),   
+   url(r'^api/script/(\d+)/edit/$', ScriptResource(permitted_methods=('POST'), ).edit),   
+   url(r'^api/script/(\d+)/delete/$', ScriptResource(permitted_methods=('POST'), ).delete),   
+   url(r'^api/script/(\d+)/get/$', ScriptResource(permitted_methods=('GET'), ).read),   
+   
+   url(r'^api/state/(\d+)/delete/$', StateResource(permitted_methods=('POST',),).delete),  
+   url(r'^api/state/new/$', StateResource(permitted_methods=('POST',),).create),
+   url(r'^api/state/(\d+)/edit/$', StateResource(permitted_methods=('POST',),).edit),
+   url(r'^api/state/(\d+)/get/$', StateResource(permitted_methods=('GET',),).read),
+   url(r'^api/state/(\d+)/add_items/$', StateResource(permitted_methods=('POST',),).add_items),
+   url(r'^api/state/(\d+)/remove_items/$', StateResource(permitted_methods=('POST',),).remove_items),
+   
+   url(r'^api/login/$', Auth(permitted_methods=('POST'), )._login),   
+   url(r'^api/get_users/$', Auth(permitted_methods=('GET'), ).get_users),   
+   url(r'^api/add_user/$', Auth(permitted_methods=('GET'), ).add_user),   
+      
+)
---
+++ src/dam/mprocessor/schedule.py
@@ -0,0 +1,118 @@
+from mediadart import log
+
+class Schedule:
+    """
+        Manages action states across execution. An action can be in 5 states:
+            <ready>: if it can be run
+            <wait>:  if it depends on an running action
+            <done>:  if it has finished running
+            <fail>:  if it produced an error
+            <cancelled>: if it depends on a failed action.
+
+        When an action is <ready> its index is in the set self.ready
+
+        When an action is in <wait> the list self.actions[action]['wait'] holds the
+        name of the actions to be waited upon.
+
+        An action is in <done>, <fail> or <cancelled> when it is not present in
+        self.actions.
+    """
+    def __init__(self, dag, target, sorted=True):
+        self.target = target   # used only when logging
+        self.dag = dag
+        a = self.action_list = self.dag.sort(sorted)
+        self.ready = set(range(len(a)))
+        self.actions = dict([(x, {'index':y, 'wlist':[]}) for (x,y) in zip(a, xrange(len(a)))])
+
+    def _cb_set_to_wait(self, node, root):
+        "set all actions dependent on root to wait"
+        if node.name != root and node.name in self.actions:
+            if root not in self.actions[node.name]['wlist']:
+                self.actions[node.name]['wlist'].append(root)
+            r = self.actions[node.name]['index']
+            if r in self.ready:
+                self.ready.remove(r)
+
+    def _cb_set_to_ready(self, node, root):
+        "mark all actions dependent on root (which just completed) as ready to run"
+        action = self.actions.get(node.name, None)
+        if action and root in action['wlist']:
+            action['wlist'].remove(root)
+            if not action['wlist']:
+                self.ready.add(action['index'])
+
+    def _cb_remove(self, node, user_data):
+        "lo stato non e' ready di sicuro del nodo"
+        log.info('target %s: cancelling %s on failed %s' % (self.target, node.name, user_data['failed_action']))
+        if node.name in self.actions:
+            del self.actions[node.name]
+            if node.name != user_data['failed_action']:
+                user_data['deleted'].append(node.name)
+
+    def action_to_run(self):
+        """Return the first action ready to run.
+           If all actions are done, failed, or cancelled, returns None
+           If no action is ready to run returns ''
+        """
+        #log.debug("action_to_run item=%s, ready=%s" % (self.target, len(self.ready)))
+        if not self.actions:
+            return None
+        if not self.ready:
+            #log.debug('nothing ready')
+            return ''
+        else:
+            r = min(self.ready)
+            action = self.action_list[r]
+            log.info('#### %s: run %s' % (self.target, action))
+            self.ready.remove(r)
+            self.dag.visit(action, self._cb_set_to_wait, action)
+            #self.show()
+            return action
+
+    def done(self, action):
+        log.debug('#### %s: done %s' % (self.target, action))
+        del self.actions[action]
+        self.dag.visit(action, self._cb_set_to_ready, action)
+        #self.show()
+
+    def fail(self, action):
+        "delete action and all actions dependent on it. Returns the number of actions deleted"
+        log.debug('#### %s: fail %s' % (self.target, action))
+        user_data = {'failed_action':action, 'deleted':[]}
+        self.dag.visit(action, self._cb_remove, user_data)
+        #self.show()
+        return user_data['deleted']
+
+    def show(self):
+        ret = "sched %s: " % self.target
+        for action in self.action_list:
+            if action not in self.actions:
+                ret += "%s:k, " % action    # done, failed or cancelled
+            elif self.actions[action]['index'] in self.ready:
+                ret += "%s:=, " % action    # ready
+            elif not self.actions[action]['wlist']:
+                ret += "%s:@, " % action    # running
+            else:
+                ret += ("%s:<%s>, " % (action, '-'.join(self.actions[action]['wlist'])))     # waiting
+        log.debug(ret)
+
+    def __str__(self):
+        return "schedule(%s)" % self.target
+
+
+# Test
+if __name__=='__main__':
+    from dam.mprocessor.pipeline import DAG
+    from dam.mprocessor.make_plugins import pipeline
+    def main():
+        p = DAG(pipeline)
+        p.show()
+        s = Schedule(p, 'dddd')
+        x1 = s.action_to_run()
+        x2 = s.action_to_run()
+        s.done(x2)
+        x3 = s.action_to_run()
+        x4 = s.action_to_run()
+        s.fail(x1)
+        x5 = s.action_to_run()
+    main()
