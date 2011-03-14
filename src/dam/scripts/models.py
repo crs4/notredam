20a2d7d1e2d165a2029c0c42e15e9ec6da29de99
mauro <mauro@localhost>
2011-03-14 09:34:11 +0100

    fixing fixtures

--- src/dam/scripts/models.py
+++ src/dam/scripts/models.py
@@ -37,47 +37,10 @@ thumbnail = {
 }
 
 
-action_audio = {
-    'extract_original': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':[],
-    },
-
-    'extract_orig_xmp': {
-        'script_name':  'extract_xmp',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':[],
-    },
-
-    'preview_audio': {
-        'script_name':  'adapt_audio',
-        'params' : {
-            'source_variant': 'original',
-            'output_variant': 'preview',
-            'output_preset': 'MP3',
-            'audio_bitrate_b': '128',
-            'audio_rate': 44100,
-        },
-        'in':[],
-        'out':[],
-    },
-
-    'extract_preview': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'preview',
-        },
-        'in':[],
-        'out':[],
-    },
-}
+action_audio = {"preview":{"params":{"source_variant":"original","output_variant":"preview","output_preset":"MP3","audio_rate":"44100","audio_bitrate_kb":"128"},"in":["ext-gen155","ext-gen157"],"out":["ext-gen153"],"script_name":"adapt_audio","x":465,"y":193,"label":"preview"},
+"extract_orig":{"params":{"source_variant":"original"},"in":[],"out":["ext-gen155"],"script_name":"extract_basic","x":26,"y":200,"label":"extract_basic"},
+"extarct_preview":{"params":{"source_variant":"preview"},"in":["ext-gen153"],"out":[],"script_name":"extract_basic","x":916,"y":189,"label":"extract_preview"},
+"extract_xmp":{"params":{"source_variant":"original"},"in":[],"out":["ext-gen157"],"script_name":"extract_xmp","x":17,"y":357,"label":"extract_xmp"}}
 
 action_short = {
     'extract_original': {
@@ -114,228 +77,37 @@ action_short = {
 }
 
 
-action_video = {
-    'extract_original': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':['fe'],
-    },
-
-    'extract_orig_xmp': {
-        'script_name':  'extract_xmp',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':['fx'],
-    },
-
-    'thumbnail': {
-        'script_name':  'extract_frame',
-        'params' : {
-            'source_variant': 'original',
-            'output_variant': 'thumbnail',
-            'output_extension': '.jpg',
-            'frame_w': '100',
-            'frame_h': '100',
-            'position': '25',
-        },
-        'in':['fe', 'fx'],
-        'out':['thumbnail'],
-    },
-
-    'extract_thumbnail': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'thumbnail',
-        },
-        'in':['thumbnail'],
-        'out':[],
-    },
-
-    'preview': {
-        'script_name':  'adapt_video',
-        'params' : {
-            'source_variant': 'original',
-            'output_variant': 'preview',
-            'output_preset': 'FLV',
-            'video_width': '300',
-            'video_height': '300',
-            'video_bitrate_b': '640000',
-            'video_framerate': '25/2',
-            'audio_bitrate_kb': '128',
-            'audio_rate': '44100',
-        },
-        'in':['fe', 'fx'],
-        'out':['preview'],
-    },
-
-    'extract_preview': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'preview',
-        },
-        'in':['preview'],
-        'out':[],
-    },
-}
+action_video = {"extract_xmp":{"params":{"source_variant":"original"},"in":[],"out":["ext-gen282","ext-gen284"],"script_name":"extract_xmp","x":14,"y":177,"label":"extract_xmp"},
+"extract_orig":{"params":{"source_variant":"original"},"in":[],"out":["ext-gen286","ext-gen288"],"script_name":"extract_basic","x":9,"y":342,"label":"extract_basic"},
+"thumbnail":{"params":{"source_variant":"original","output_variant":"thumbnail","frame_w":"100","frame_h":"100","position":"25","output_extension":".jpg"},"in":["ext-gen282","ext-gen286"],"out":["ext-gen290"],"script_name":"extract_frame","x":492,"y":334,"label":"thumbnail"},
+"extract_thumbnail":{"params":{"source_variant":"thumbnail"},"in":["ext-gen290"],"out":[],"script_name":"extract_basic","x":922,"y":338,"label":"extract_thumbnail"},
+"preview":{"params":{"source_variant":"original","output_variant":"preview","preset":"FLV","video_bitrate_b":"640000","audio_bitrate_kb":"128","video_framerate":"25/2","audio_rate":"44100","video_height":"300","video_width":"300"},"in":["ext-gen284","ext-gen288"],"out":["ext-gen292"],"script_name":"adapt_video","x":483,"y":171,"label":"preview"},
+"extract_preview":{"params":{"source_variant":"preview"},"in":["ext-gen292"],"out":[],"script_name":"extract_basic","x":905,"y":166,"label":"extract_preview"}}
 
 
-action_image = {
-    'extract_original': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':['fe'],
-    },
+action_image = {"thumbnail":{"params":{"source_variant":"original", "output_variant":"thumbnail", "resize_h":"100","resize_w":"100","actions":"resize","output_extension":".jpg"},"in":["ext-gen375","ext-gen390"],"out":["ext-gen371"],"script_name":"adapt_image","x":511,"y":178,"label":"thumbnail" },
 
-    'extract_orig_xmp': {
-        'script_name':  'extract_xmp',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':['fx'],
-    },
+"extract_thumbnail":{"params":{"source_variant":"thumbnail"},"in":["ext-gen371"],"out":[],"script_name":"extract_basic","x":1040,"y":176,"label":"extract_basic"},
 
-    'thumbnail_image':{
-        'script_name': 'adapt_image', 
-        'params':{
-            'actions':['resize'],
-            'resize_h':100,
-            'resize_w': 100,
-            'source_variant': 'original',
-            'output_variant': 'thumbnail',
-            'output_extension' : '.jpg'        
-            },
-         'in': ['fe', 'fx'],
-         'out':['thumbnail']    
-        
-        
-    },
+"extract_xmp_orig":{"params": {"source_variant":"original"},"in":[],"out":["ext-gen384","ext-gen387","ext-gen390"],"script_name":"extract_xmp","x":21,"y":459,"label":"extract_xmp"},
 
-    'extract_thumbnail': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'thumbnail',
-        },
-        'in':['thumbnail'],
-        'out':[],
-    },
+"fullscreen":{"params":{"source_variant":"original","output_variant":"fullscreen","resize_h":"800","resize_w":"800","actions":"resize","output_extension":".jpeg"},"in":["ext-gen377","ext-gen384"],"out":["ext-gen373"],"script_name":"adapt_image","x":519,"y":570,"label":"fullscreen"},
 
-    'preview_image': {
-        'script_name': 'adapt_image', 
-        'params':{
-            'actions':['resize'],
-            'resize_h':300,
-            'resize_w': 300,
-            'source_variant': 'original',
-            'output_variant': 'preview',
-            'output_extension' : '.jpeg'        
-            },
-         'in': ['fe', 'fx'],
-         'out':['preview']    
-        },
-        
-    'extract_preview': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'preview',
-        },
-        'in':['preview'],
-        'out':[],
-    },
-    
-    'fullscreen_image': {
-        'script_name': 'adapt_image', 
-        'params':{
-            'actions':['resize'],
-            'resize_h':800,
-            'resize_w': 800,
-            'source_variant': 'original',
-            'output_variant': 'fullscreen',
-            'output_extension' : '.jpeg'        
-            },
-         'in': ['fe', 'fx'],
-         'out':['fullscreen']    
-    },
+"extract_orig":{"params":{"source_variant":"original"},"in":[],"out":["ext-gen375","ext-gen377","ext-gen379"],"script_name":"extract_basic","x":19,"y":332,"label":"extract_basic"},
 
-    'extract_full': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'fullscreen',
-        },
-        'in':['fullscreen'],
-        'out':[],
-    },
-}
+"extract_fullscreen":{"params":{"source_variant":"fullscreen"},"in":["ext-gen373"],"out":[],"script_name":"extract_basic","x":1042,"y":564,"label":"extract_basic"},
 
-action_pdf = {
-    'extract_original': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':['fe'],
-    },
+"extract_preview":{"params":{"source_variant":"preview"},"in":["ext-gen381"],"out":[],"script_name":"extract_basic","x":1024,"y":348,"label":"extract_basic"},
 
-    'extract_orig_xmp': {
-        'script_name':  'extract_xmp',
-        'params' : {
-            'source_variant': 'original',
-        },
-        'in':[],
-        'out':['fx'],
-    },
+"preview":{"params":{"source_variant":"original","output_variant":"preview","resize_h":"300","resize_w":"300","actions":"resize","output_extension":".jpeg"},"in":["ext-gen379","ext-gen387"],"out":["ext-gen381"],"script_name":"adapt_image","x":508,"y":353,"label":"preview"}}
 
-    'thumbnail': {
-        'script_name':  'pdfcover',
-        'params' : {
-            'source_variant': 'original',
-            'output_variant': 'thumbnail',
-            'output_extension': '.jpg',
-            'max_size': '100',
-        },
-        'in':['fe', 'fx'],
-        'out':['thumbnail'],
-    },
 
-    'extract_thumbnail': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'thumbnail',
-        },
-        'in':['thumbnail'],
-        'out':[],
-    },
-
-    'preview': {
-        'script_name':  'pdfcover',
-        'params' : {
-            'source_variant': 'original',
-            'output_variant': 'preview',
-            'output_extension': '.jpg',
-            'max_size': '300',
-        },
-        'in':['fe', 'fx'],
-        'out':['preview'],
-    },
-
-    'extract_preview': {
-        'script_name':  'extract_basic',
-        'params' : {
-            'source_variant': 'preview',
-        },
-        'in':['preview'],
-        'out':[],
-    },
-}
+action_pdf = {"extract_xmp":{"params":{"source_variant":"original"},"in":[],"out":["ext-gen173","ext-gen175"],"script_name":"extract_xmp","x":14,"y":178,"label":"extract_xmp"},
+"extract_orig":{"params":{"source_variant":"original"},"in":[],"out":["ext-gen177","ext-gen179"],"script_name":"extract_basic","x":10,"y":442,"label":"extract_basic"},
+"thumbnail":{"params":{"source_variant":"original","output_variant":"thumbnail","maxsize":"300","output_extension":".jpg"},"in":["ext-gen173","ext-gen177"],"out":["ext-gen181"],"script_name":"pdfcover","x":551,"y":395,"label":"thumbnail"},
+"extract_thumb":{"params":{"source_variant":"thumbnail"},"in":["ext-gen181"],"out":[],"script_name":"extract_basic","x":1036,"y":385,"label":"extract_basic"},
+"preview":{"params":{"source_variant":"original","output_variant":"preview","maxsize":"300","output_extension":".jpg"},"in":["ext-gen175","ext-gen179"],"out":["ext-gen183"],"script_name":"pdfcover","x":560,"y":184,"label":"preview"},
+"extract_preview":{"params":{"source_variant":"preview"},"in":["ext-gen183"],"out":[],"script_name":"extract_basic","x":1017,"y":185,"label":"extract_basic"}}
 
 DEFAULT_PIPELINE = [{
                      'name': 'action_audio', 
