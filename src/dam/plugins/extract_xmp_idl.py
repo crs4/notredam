4c5adf1d76a913edf5a2bc401266694c76e6d1e4
Fabrizio Solinas <fabrizio.solinas83@gmail.com>
2011-04-26 14:12:00 +0200

    change Version in menu.js

---
+++ src/dam/plugins/extract_xmp_idl.py
@@ -0,0 +1,23 @@
+#
+# Interface definition for adapt_audio: used by the GUI
+#
+
+from dam.plugins.common.utils import get_variants
+
+def inspect(workspace):
+    variants = get_variants(workspace)
+    return {
+    'name': __name__,
+    'params':[
+        {   
+            'name': 'source_variant_name',
+            'fieldLabel': 'Input Rendition',
+            'xtype': 'select',
+            'values': variants,
+            'value': variants[0],
+            'description': 'input-variant',                
+            'help': ''
+        }]
+     
+    }
+    
