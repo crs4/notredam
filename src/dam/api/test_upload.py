312a5e4171166264656af3882540450e53ee7db4
mauro <mauro@localhost>
2011-07-05 14:10:13 +0200

    fixed test_upload.py: now it requires an arg file path(python test_upload.py /file/to/upload)

--- src/dam/api/test_upload.py
+++ src/dam/api/test_upload.py
@@ -8,10 +8,22 @@ from django.test.client import Client
 from dam.api.models import *
 from dam.workspace.models import DAMWorkspace
 from dam.repository.models import Item,  Component
-
+import os
 from dam.core.dam_repository.models import Type
+import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print """No file passed. 
+        Typical use:
+        
+            python manage.py loaddata api/fixtures/test_data.json
+            python test_upload.py /path/file/to/upload
+        
+        """
+        sys.exit(2)
+        
+    file_path = sys.argv[1]
     client = Client()
     secret_obj = Secret.objects.get(pk = 1)  
     secret = secret_obj.value
@@ -27,7 +39,7 @@ if __name__ == "__main__":
     response = client.post('/api/item/new/', params,)                
     resp_dict = simplejson.loads(response.content)
 
-    file = open('/home/mauro/work/dam_trunk/src/dam/files/images/logo_blue.jpg')
+    file = open(file_path)
     params = _get_final_parameters(api_key = api_key, secret = secret, user_id = user_id, kwargs = {'workspace_id': 1,  'rendition_id':1})                
     params['files_to_upload'] = file
     
