62237e5e14b58af56c851ee7e13752a7155503ca
mdrio <mauro@crs4.it>
2011-09-09 17:32:47 +0200

    radio button in supported languages gui

---
+++ src/website/notreDAM/NotreDAM/GuideStep4_files/GuideStep4.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20091214
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,369),url:'GuideStep4_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'GuideStep4_files/stroke_1.png'},{rect:new IWRect(2,-2,190,4),url:'GuideStep4_files/stroke_2.png'},{rect:new IWRect(192,-2,4,4),url:'GuideStep4_files/stroke_3.png'},{rect:new IWRect(192,2,4,369),url:'GuideStep4_files/stroke_4.png'},{rect:new IWRect(192,371,4,4),url:'GuideStep4_files/stroke_5.png'},{rect:new IWRect(2,371,190,4),url:'GuideStep4_files/stroke_6.png'},{rect:new IWRect(-2,371,4,4),url:'GuideStep4_files/stroke_7.png'}],new IWSize(194,373))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('GuideStep4_files/GuideStep4Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');adjustLineHeightIfTooBig('id14');adjustFontSizeIfTooBig('id14');adjustLineHeightIfTooBig('id15');adjustFontSizeIfTooBig('id15');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
