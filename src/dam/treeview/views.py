43a83b071b7aee2168cb7686cba3f2c376aa8b44
fabrizio solinas83 <fabrizio.solinas83@gmail.com>
2011-10-24 16:19:49 +0200

    fix merge

--- src/dam/treeview/views.py
+++ src/dam/treeview/views.py
@@ -101,12 +101,7 @@ def _add_keyword(request, node, label, workspace):
     if (cls in valid_object_node_classes):
         # The KB object name will override the label
         # FIXME: check that the provided label is equal to obj name?
-<<<<<<< local
-        print 'request.POST[\'kb_object\']: %s' %request.POST['kb_object']
-        obj = KBObject.objects.get(id=request.POST['kb_object'])
-=======
         obj = KBObject.object.get(id=request.POST['kb_object'])
->>>>>>> other
         label = obj.name
 
     node = Node.objects.add_node(node, label, workspace, cls, request.POST.get('add_metadata_parent', False), kb_object=obj)
