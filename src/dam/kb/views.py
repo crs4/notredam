adb973e5c75effbf3aad04b3968c2b4ef4679a77
Alceste Scalas <alceste@muvara.org>
2011-10-29 12:00:07 +0200

    Use tinykb.errors module (instead of tinykb.exceptions)

--- src/dam/kb/views.py
+++ src/dam/kb/views.py
@@ -31,7 +31,7 @@ from django.utils import simplejson
 import tinykb.access as kb_access
 import tinykb.session as kb_ses
 import tinykb.classes as kb_cls
-import tinykb.exceptions as kb_exc
+import tinykb.errors as kb_exc
 import tinykb.attributes as kb_attrs
 import util
 
