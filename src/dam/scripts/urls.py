17e8c5cb43ee6b2e453acb7a89d30a9f832c7f03
mlclemente <clem@crs4.it>
2012-01-30 12:34:08 +0100

    fixed a bug in add_items_to_workspace

---
+++ src/dam/scripts/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^get_scripts/', 'dam.scripts.views.get_scripts'),
+    (r'^get_rotation_script/', 'dam.scripts.views.get_rotation_script'),
+    (r'^edit_script/', 'dam.scripts.views.edit_script'),
+    (r'^delete_script/', 'dam.scripts.views.delete_script'),
+    (r'^get_actions/', 'dam.scripts.views.get_actions'),
+    (r'^run_script/', 'dam.scripts.views.run_script'),
+    (r'^script_monitor/', 'dam.scripts.views.script_monitor'),
+    (r'^script_editor/(.+)/$', 'dam.scripts.views.editor'),
+    (r'^script_editor/$', 'dam.scripts.views.editor'),
+	(r'^get_events/$', 'dam.scripts.views.get_events'),
+	(r'^get_types/$', 'dam.scripts.views.get_media_types'),
+    (r'^get_failures_info/$', 'dam.scripts.views.get_failures_info'),
+    
+    
+)
