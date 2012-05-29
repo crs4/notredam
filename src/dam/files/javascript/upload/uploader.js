63b9b6d10428d94b0e8dc7ffa3f342b666f358ed
clem <clem@localhost>
2012-05-29 13:38:25 +0200

    fixed a bug

--- src/dam/files/javascript/upload/uploader.js
+++ src/dam/files/javascript/upload/uploader.js
@@ -170,9 +170,6 @@ function upload_dialog(cfg){
 			success: function(){
 				cfg.after_upload(mysession);
 			}
-			failure: function(){
-                                Ext.MessageBox.alert(gettext('Error'), gettext('File uploading failed'));
-			}
 			
 			});
              }
