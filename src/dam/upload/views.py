7427a7235dbfaa5de7924d470811df73f0544a5c
clem <clem@localhost>
2012-09-04 14:32:20 +0200

    changed file_name

--- src/dam/upload/views.py
+++ src/dam/upload/views.py
@@ -463,10 +463,10 @@ def _write_file(input_file, output_file_path):
 def _upload_item(file_name, file_raw,  variant, user, tmp_dir, workspace, session_finished = False):
     logger.debug('_upload_item %s in %s'%(file_name, tmp_dir))
     file_name = new_id() + '_' + file_name    
-    
+    myout=  os.path.join(tmp_dir, file_name)
     if not isinstance(file_name, unicode):
         file_name = unicode(file_name, 'utf-8')
-    _write_file(file_raw, os.path.join(tmp_dir, file_name))
+    _write_file(file_raw, file_name)
     
 
     
