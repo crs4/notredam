788e4fb54fa39b9002dae5ec3751ac3c19d7520a
Alceste Scalas <alceste@crs4.it>
2013-01-26 18:41:12 +0100

    Add xmp_embedder to available Celery tasks

--- src/dam/celeryconfig.py
+++ src/dam/celeryconfig.py
@@ -2,4 +2,5 @@ BROKER_URL = "amqp://guest:guest@localhost:5672//"
 CELERY_RESULT_BACKEND = "amqp"
 CELERY_IMPORTS = ("dam.mprocessor.processor", 
                   "dam.mprocessor.servers.generic_cmd",
-                  "dam.mprocessor.servers.xmp_extractor")
+                  "dam.mprocessor.servers.xmp_embedder",
+                  "dam.mprocessor.servers.xmp_extractor",)
