e620b4b1796505999b51b8ad22a075be366500d4
Alceste Scalas <alceste@crs4.it>
2013-04-03 12:59:39 +0200

    Check whether process.start_date is NULL before strftime()

--- src/dam/scripts/views.py
+++ src/dam/scripts/views.py
@@ -252,7 +252,9 @@ def _script_monitor(workspace):
                  'items_completed': items_completed,
                  'progress':progress,
                  'type': ','.join([trigger.name for trigger in process.pipeline.triggers.all()]),                 
-                 'start_date': process.start_date.strftime("%d/%m/%y %I:%M:%S"),
+                 'start_date': ((process.start_date is not None)
+                                and process.start_date.strftime("%d/%m/%y %I:%M:%S")
+                                or None),
                  'launched_by': process.launched_by.username,
                  'items_failed': items_failed
                  }
