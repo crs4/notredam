746b6b59f10613927e065cca16844b73a0e6632b
Alceste Scalas <alceste@crs4.it>
2013-04-04 16:50:35 +0200

    Check whether a component URI is None

--- src/dam/repository/models.py
+++ src/dam/repository/models.py
@@ -302,7 +302,7 @@ class Item(AbstractItem):
 
         
         for c in self.component_set.filter(workspace__in = workspaces).exclude(variant__name = 'original'):                
-            if c is None:
+            if (c is None) or (c.uri is None):
                 # The component may be None e.g. when an upload fails
                 continue
             try:
