55da00cf562e41072d5e07af258d6bbb5a0a3215
clem <clem@localhost>
2013-11-28 10:47:36 +0100

    minor changes

--- src/dam/config.py
+++ src/dam/config.py
@@ -20,11 +20,11 @@ SAFE_MODE = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dam_db',                      # Or path to database file if using sqlite3.
-        'USER': 'outreach',                      # Not used with sqlite3.
-        'PASSWORD': 'outatcrs4',                  # Not used with sqlite3.
-        'HOST': '156.148.22.16',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'dam',                      # Or path to database file if using sqlite3.
+        'USER': 'dam',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
     #'default': {
         #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
